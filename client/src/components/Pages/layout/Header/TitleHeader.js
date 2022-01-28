9cbcad8439951c016feea35494609c3639191303
khosrora <khosrora153333@gmail.com>
2022-01-28 12:00:02 +0330

    front ==> set Header

--- client/src/components/Pages/layout/Header/TitleHeader.js
+++ client/src/components/Pages/layout/Header/TitleHeader.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import TypeText from "./../../../utilities/TypeText"
 
 
 const TitleHeader = () => {
     return (
-        <div className="title-header">
-            
+        <div className="desc-header">
+            <div className="title">
+                <div className="details-title">
+                    <h1>کاری از رابا</h1>
+                    <span>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ</span>
+                    <p>
+                        ا می طلبد، تا با نرم افزارها شناخت بیشتری را
+                        برای طراحان رایانه ای علی الخصوص طراحان خلاقی،
+                        و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در ای
+                        ن صورت می توان امید داشت که تمام و دشواری مو
+                        جود در ارائه راهکارها، و شرایط سخت تایپ به پ
+                        ایان رسد و زمان مورد نیاز شامل حروفچینی دستا
+                        وردهای اصلی، و جوابگوی سوالات پیوسته اهل د
+                        نیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                    </p>
+                </div>
+            </div>
+            <div className="back-desc-header"></div>
         </div>
     );
 };
